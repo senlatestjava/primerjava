40889c9bc0f305cf37c93ea1b7e5180f8edc83b2
andrei_kotsko <andrei_kotsko@senla.eu>
2018-05-15 10:03:19 +0300

    teste

---
+++ src/test/java/WebDriverSettings.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class WebDriverSettings {
+    public ChromeDriver driver;
+
+    @BeforeTest
+    public void stUp(){
+        System.setProperty("webdriver.chrome.driver", "c:\\Program Files\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+    }
+
+
+
+    @AfterTest
+    public void close(){
+        driver.quit();
+    }
+
+}
